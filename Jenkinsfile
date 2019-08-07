fda132ba43a5229d41252d04ccf569e3c836083d
Ashok Shingade <ashingade@jetblue.com>
2019-08-08 00:58:07 +0530

    Added pipeline configuration

---
+++ Jenkinsfile
@@ -0,0 +1,22 @@
+pipeline {
+   agent any
+   environment {
+      REGION =''
+   }
+
+   stages {
+      stage('user input') {
+         steps {
+            script {
+               timeout ( time: 10, unit: "MINUTES" )  {
+                  REGION=input message: 'Select Region',  parameters: [choice(choices: 'eu-west-3\nus-west-2', description: '', name: 'Region:')]
+                  if (REGION == "eu-west-3" ) {
+                    sh "python --version"
+                  }
+                }
+            }
+         }
+      }
+   }
+}
+
---
+++ lambda_function.py
@@ -0,0 +1,34 @@
+import boto3
+
+
+ # Get list of regions
+ec2_client = boto3.client('ec2')
+regions = [region['RegionName']
+        for region in ec2_client.describe_regions()['Regions']]
+
+# Iterate over each region
+for region in regions:
+    ec2 = boto3.resource('ec2', region_name=region)
+
+    print("Region:", region)
+    # Get only running instances
+    ids = []
+    instances = ec2.instances.filter(
+        Filters=[{'Name': 'instance-state-name',
+                    'Values': ['running']}])
+    for instance in instances:
+        print("Running Instances: ", instance.id)
+        ids.append(instance.id)
+
+        if len(ids) >= 1:
+            print("Changing tags for %d instances" % len(ids))
+
+            ec2.create_tags(
+                Resources=ids,
+                Tags=[
+                    {
+                        'Key': 'Env',
+                        'Value': 'Production'
+                    }
+                ]
+            )

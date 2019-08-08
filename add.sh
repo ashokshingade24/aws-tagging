8ca09eb14a2c48bfd290af889f62a927c4c05680
ashokshingade24 <ashokshingade24@gmail.com>
2019-08-09 03:08:30 +0530

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # insert/update hosts entry
-host_name="${Hostname}"
+host_name="${Host}"
 # find existing instances in the host file and save the line numbers
 matches_in_hosts="$(grep -n $host_name /etc/hosts | cut -f1 -d:)"
 host_entry="${ip_address} ${host_name}"

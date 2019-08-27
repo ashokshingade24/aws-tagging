e50f763f6ad511aaaf9d9787e4973424e60ad348
ashokshingade24 <ashokshingade24@gmail.com>
2019-08-28 00:29:21 +0530

    Update add.sh

--- add.sh
+++ add.sh
@@ -3,7 +3,7 @@
 # insert/update hosts entry
 host_name=$1
 # find existing instances in the host file and save the line numbers
-matches_in_hosts="$(grep -n $host_name /tmp/hosts | cut -f1 -d:)"
+matches_in_hosts="$(grep -n $host_name /etc/hosts | cut -f1 -d:)"
 host_entry="${host_name}"
 
 if [ ! -z "$matches_in_hosts" ]
@@ -12,10 +12,10 @@ then
     # iterate over the line numbers on which matches were found
     while read -r line_number; do
         # replace the text of each line with the desired host entry
-        sudo sed -i '' "${line_number}s/.*/${host_entry} /" /tmp/hosts
+        sudo sed -i '' "${line_number}s/.*/${host_entry} /" /etc/hosts
     done <<< "$matches_in_hosts"
 else
     echo "Adding new hosts entry."
-    echo "$host_entry" | tee -a /tmp/hosts > /dev/null
+    echo "$host_entry" | sudo tee -a /etc/hosts > /dev/null
 fi
-cat /tmp/hosts
+cat /etc/hosts
